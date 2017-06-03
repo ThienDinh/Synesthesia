123aa41d43cf751b09a83dab3160b67e11023ee4
Thien Dinh <Thien Dinh>
2017-06-03 18:35:18 -0400

    Add color alphabet into HTML file.

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.listen(80);
-app.use('/', express.static('.'));
+app.use('/lib', express.static('./node_modules/d3/build'));
+app.use('/lib', express.static('./static'));
+
+app.get('/', function(req, res) {
+	fs.readFile('static/index.html', function(err, data) {
+		if(!err)
+			res.send(data.toString());
+		else
+			res.status(505);
+	})
+})
